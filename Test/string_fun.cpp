e7d98e685e66de93839cdbb4fc52d337adfea392
Michal Malek <michal.malek98@wp.pl>
2021-03-10 10:56:56 +0100

    Add files via upload

--- Test/string_fun.cpp
+++ Test/string_fun.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+void found_seq(char s1[], char s2[]) {
+    cout << strstr(s1, s2) << endl;
+}
+
+void get_index(char s1[], char s2[]) {
+    cout << strspn(s1, s2) << endl;
+}
+
 int main() {
     char str[] = "Hello World";
 
@@ -11,6 +19,12 @@ int main() {
         cout << "Letter: " << str[i] << " -> " << (int) str[i] << endl;
         i++;
     }
+
+    char s2[] = "ma";
+    char s1[] = "ala ma kota";
+    found_seq(s1, s2);
+    get_index(s1, s2);
+
 
     return 0;
 }
