dc48a66a11c5cd3233d2807fc47f34b84e454dbd
Michal Malek <michal.malek98@wp.pl>
2021-03-10 10:06:56 +0100

    namespace with function

--- test.cpp
+++ test.cpp
@@ -6,6 +6,10 @@ using namespace std;
 namespace example  {
     int a = 5;
     int b = 10;
+
+    void test() {
+      cout << "test method" << endl;
+    }
 }
 
 
@@ -16,6 +20,7 @@ int add(int num1, int num2) {
 }
 
 int main() {
+    test();
     cout << add(a, b) << endl;
     return 0;
 }
