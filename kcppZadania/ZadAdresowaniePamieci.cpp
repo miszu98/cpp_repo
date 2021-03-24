8af3353cf6dcc602c9581e40a8e91006d0d2ad5e
Michal Malek <michal.malek98@wp.pl>
2021-03-24 10:44:45 +0100

    Add files via upload

---
+++ kcppZadania/ZadAdresowaniePamieci.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    int a, b, c;
+    a = 10;
+    b = 20;
+    c = 30;
+
+    cout << "Zmienna a: " << &a << " Wartosc: " << a <<  endl;
+
+    cout << "Zmienna b: " << &b << " Wartosc: " << b <<  endl;
+
+    cout << "Zmienna c: " << &c << " Wartosc: " << c <<  endl;
+
+    return 0;
+}
