501e980759591501d0802e84f5fb726c0e776ac9
Michal Malek <michal.malek98@wp.pl>
2021-04-07 10:20:09 +0200

    Add files via upload

---
+++ kcppZadania/ZadParzysta.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+bool evenNumber(int num) {
+    if (num % 2 == 0) {
+        return true;
+    } return false;
+}
+
+bool evenNumberBits(int num) {
+    if (num & 1) {
+        return false;
+    } return true;
+}
+
+int main() {
+    cout << evenNumber(3) << endl;
+    cout << evenNumberBits(3) << endl;
+
+}
+
+
