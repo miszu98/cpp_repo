1913bf43586b8622188509e18168f29508c111cd
Michal Malek <michal.malek98@wp.pl>
2021-04-07 10:07:04 +0200

    Add files via upload

---
+++ kcppZadania/parzyste.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+bool evenNumber(int num) {
+    if (num % 2 == 0) {
+        return true;
+    } return false;
+}
+
+int main() {
+    cout << evenNumber(2) << endl;
+
+}
+
+
